e923ea0bc4ff3de29e460b68c7b58ef809b50fda
wen <yanwei4682@163.com>
2017-11-20 15:14:07 +0800

    '111'

---
+++ wenda.py
@@ -0,0 +1,12 @@
+﻿from flask import Flask,url_for,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<name>')
+def index(name=None):
+    return render_template('index.html',name=name)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
